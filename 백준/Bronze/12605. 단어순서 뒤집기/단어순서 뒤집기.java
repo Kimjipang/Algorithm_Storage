fec1317899b93b778d9eadf368c9b9837cdb0998
김지환 <91196025+Kimjipang@users.noreply.github.com>
2024-06-05 12:28:30 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 156 ms, Memory: 16112 KB -BaekjoonHub

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    /*
+    
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -10,12 +13,12 @@ public class Main {
 
         for(int i = 1; i <= cases; i++) {
             String[] words = br.readLine().split(" ");
-            String sentences = "";
-
+            StringBuilder sentences = new StringBuilder();
+            
             for(int j = words.length - 1; j >= 0; j--) {
-                sentences += words[j];
+                sentences.append( words[j]);
                 if(j != 0) {
-                    sentences += " ";
+                    sentences.append(" ");
                 }
             }
             System.out.println("Case #" + i + ": " + sentences);
