d793f965764b38df337fd8f90f451ec762e3d9de
김지환 <91196025+Kimjipang@users.noreply.github.com>
2024-06-14 13:13:01 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 368 ms, Memory: 20492 KB -BaekjoonHub

--- 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.java
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.java
@@ -34,12 +34,11 @@ public class Main {
         for(int i = 0; i < len - 1; i++) {
             int count = 0;
             for(int j = i + 1; j < len; j++) {
-                if(hanzo[i] >= hanzo[j]) {
+                if(hanzo[i] > hanzo[j]) {
                     count++;
+                    continue;
                 }
-                else {
-                    break;
-                }
+                break;
             }
             maxKill = Math.max(count, maxKill);
         }
