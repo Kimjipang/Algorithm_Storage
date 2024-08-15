463c1cc82130ae0e76bc6c1577500eff3f49d27d
김지환 <91196025+Kimjipang@users.noreply.github.com>
2024-08-15 17:51:57 +0900

    [level 2] Title: 가장 큰 수, Time: 3443.58 ms, Memory: 405 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        
+        String[] nums = new String[numbers.length];
+        
+        int len = nums.length;
+        
+        for (int i = 0; i < len; i++) {
+            nums[i] = numbers[i] + "";
+        }
+        
+        Arrays.sort(nums, new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo(o1 + o2);
+            }
+        });
+        
+        for (int i = 0; i < len; i++) {
+            answer += nums[i];
+        }
+        
+        return answer.charAt(0) == '0' ? "0" : answer;
+    }
+}
