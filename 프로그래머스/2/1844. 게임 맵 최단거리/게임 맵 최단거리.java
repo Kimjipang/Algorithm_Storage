e118cb54716495a4f6d82b32f1fa58246896b3d1
김지환 <91196025+Kimjipang@users.noreply.github.com>
2024-08-16 14:30:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.56 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,29 +2,33 @@ import java.util.*;
 
 class Solution {
     static int n, m;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
     static boolean[][] visited;
     
     public int solution(int[][] maps) {
+        int answer = 0; 
+        
         n = maps.length;
         m = maps[0].length;
+        
         visited = new boolean[n][m];
         
-        return bfs(0, 0, maps);
+        answer = bfs(maps);
         
+        return answer;
     }
-    private static int bfs(int x, int y, int[][] maps) {
+    private static int bfs(int[][] maps) {
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {-1, 1, 0, 0};
+        
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {x, y, 1});
-        visited[x][y] = true;
+        queue.add(new int[] {0, 0, 1});
+        visited[0][0] = true;
         
-        
-        while(!queue.isEmpty()) {
-            int[] arr = queue.poll();
-            int curX = arr[0];
-            int curY = arr[1];
-            int count = arr[2];
+        while (!queue.isEmpty()) {
+            int[] position = queue.poll();
+            int curX = position[0];
+            int curY = position[1];
+            int count = position[2];
             
             if (curX == n - 1 && curY == m - 1) {
                 return count;
@@ -33,13 +37,12 @@ class Solution {
             for (int i = 0; i < 4; i++) {
                 int nextX = curX + dx[i];
                 int nextY = curY + dy[i];
-                
                 if (nextX >= 0 && nextY >= 0 && nextX < n && nextY < m) {
                     if (maps[nextX][nextY] == 1 && !visited[nextX][nextY]) {
                         queue.add(new int[] {nextX, nextY, count + 1});
                         visited[nextX][nextY] = true;
                     }
-                }
+                } 
             }
         }
         return -1;
