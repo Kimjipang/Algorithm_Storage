df24af997a01add7bb481adf50397eb3009e6b6c
김지환 <91196025+Kimjipang@users.noreply.github.com>
2024-09-30 15:08:05 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,20 +1,23 @@
-from collections import deque
+from collections import *
+
 def solution(progresses, speeds):
     answer = []
-    time = 0
     count = 0
+    time = 0
+    
     progresses = deque(progresses)
     speeds = deque(speeds)
-    while len(progresses)> 0:
-        if (progresses[0] + time*speeds[0]) >= 100:
+    
+    while progresses:
+        if progresses[0] + time * speeds[0] >= 100:
             progresses.popleft()
             speeds.popleft()
             count += 1
         else:
-            if count > 0:
+            if count != 0:
                 answer.append(count)
                 count = 0
             time += 1
+        
     answer.append(count)
-
     return answer
