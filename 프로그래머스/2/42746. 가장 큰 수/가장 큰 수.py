cec9e55c26ec7569cb4fc9dc46072171b0f77db9
김지환 <91196025+Kimjipang@users.noreply.github.com>
2024-10-08 11:36:35 +0900

    [level 2] Title: 가장 큰 수, Time: 435.21 ms, Memory: 27.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,8 +1,6 @@
+
 def solution(numbers):
-    answer = ''
-    
     str_numbers = [str(num) for num in numbers]
-    str_numbers.sort(key=lambda x : x * 3, reverse = True)
+    str_numbers.sort(key=lambda x: x*3, reverse=True)
     result = ''.join(str_numbers)
-    
     return result if int(result) != 0 else '0'
