3da2198ee8a92abb8b5c8cf98220da967da4b4eb
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-02-26 00:23:56 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.09 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        
+        for (int[] query : queries) {
+            int tmp = arr[query[0]];
+            
+            arr[query[0]] = arr[query[1]];
+            arr[query[1]] = tmp;
+        }
+        
+        return arr;
+    }
+}
