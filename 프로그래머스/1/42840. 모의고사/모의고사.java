bdda75b10816dc4303f44a408fc3da807df8b861
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-03-13 22:19:54 +0900

    [level 1] Title: 모의고사, Time: 8.03 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        /*
+        answers가 주어지고 1,2,3번 수포자 중에서 누가 제일 많이 답을 맞췄는지 계산하는 문제
+        1번 수포자 -> 1, 2, 3, 4, 5 반복
+        2번 수포자 -> 2, 1, 2, 3, 2, 4, 2, 5 반복
+        3번 수포자 -> 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 반복
+        
+        총 3번 순회를 해야 하는데, 3명의 수포자를 순회하면서 정답이랑 비교하고 정답 개수 반환
+        */
+        int[] result = new int[3];
+        int len = answers.length;
+        
+        int[][] people = {{1, 2, 3, 4, 5},
+                          {2, 1, 2, 3, 2, 4, 2, 5},
+                         {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+        
+        // 문제가 8문제라면? 
+        for (int i = 0; i < 3; i++) {
+            int num = people[i].length;
+            int count = 0;
+            
+            for (int j = 0; j < len; j++) {
+                if (answers[j] == people[i][j % num]) {
+                    count++;
+                }
+            }
+            result[i] = count;
+        }
+        
+        return countBestScorer(result).stream()
+                                    .mapToInt(Integer::intValue)
+                                    .toArray();
+    }
+    
+    private static List<Integer> countBestScorer(int[] result) {
+        // 3개 중에서 제일 높은 숫자를 찾은 후에 다시 처음부터 비교해서 카운트를 세
+        List<Integer> list = new ArrayList<>();
+        int num = -1;
+        
+        for (int i = 0; i < 3; i++) {
+            num = Math.max(result[i], num);
+        }
+        
+        for (int i = 0; i < 3; i++) {
+            if (num == result[i]) {
+                list.add(i + 1);
+            }
+        }
+        
+        return list;
+    }
+}
