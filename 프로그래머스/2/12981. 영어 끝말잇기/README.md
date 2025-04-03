a6761e267165ce8452d11b9f2d0d04432e3d2bb8
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-04-04 00:24:28 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 78.6 MB, 시간: 0.10 ms
+메모리: 79 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 18일 17:19:43
+2025년 04월 04일 00:24:23
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,26 +1,34 @@
 import java.util.*;
 
 class Solution {
-    
     public int[] solution(int n, String[] words) {
+        /*
+        words의 길이는 최대 100 O(N^2) 가능
+        
+        1. set을 하나 만들고, words를 순회
+        2-1. set에 값이 있으면 탈락자 반환
+        2-2. 없으면 set에 집어 넣음.
+        
+        */
         int[] answer = {0, 0};
         
-        Set<String> visited = new HashSet<>();
-        int len = words.length;
-               
-        visited.add(words[0]);
-        char ch = words[0].charAt(words[0].length() - 1);
+        Set<String> set = new HashSet<>();
+        set.add(words[0]);
         
-        for (int i = 1; i < len; i++) {
+        int len = words.length;
+        int count = n;
+        for (int i = 1; i < len; i++) { // words 순회
             String word = words[i];
-            char firstCh = word.charAt(0);
-            if (visited.contains(word) || firstCh != ch) {
-                answer[0] = i % n + 1;
-                answer[1] = i / n + 1;
-                return answer;
+            char last_alphabet = words[i - 1].charAt(words[i - 1].length() - 1);
+            count++;
+            
+            // 전에 나온 적이 있는 단어이거나 끝말이 이어지지 않은 단어 체크
+            if (set.contains(word) || last_alphabet != word.charAt(0)) {
+                answer[0] = (i % n) + 1;
+                answer[1] = count / n; 
+                break;
             }
-            visited.add(word);
-            ch = word.charAt(word.length() - 1);
+            set.add(word);
         }
 
         return answer;
