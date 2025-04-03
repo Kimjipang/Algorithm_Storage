a5c0cb2f4b7f54a3ec4dfa2abe686b03b98dda07
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-04-03 23:27:30 +0900

    [level 1] Title: 폰켓몬, Time: 5.27 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
 
 ### 성능 요약
 
-메모리: 11 MB, 시간: 0.74 ms
+메모리: 76.1 MB, 시간: 5.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 05일 12:46:34
+2025년 04월 03일 23:27:26
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,21 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        int len = nums.length / 2;
+        /*
+        nums의 길이는 최대 10,000이기에 O(N^2) ㄴㄴ
+        
+        [풀이]
+        1. 집합에 nums를 저장
+        2. 길이가 N/2이상이면 N/2 return
+        2-1. N/2 미만이면 집합의 길이 return
+        */
         
         Set<Integer> set = new HashSet<>();
         
         for (int num : nums) {
             set.add(num);
         }
-        int size = set.size();
         
-        if (len < size) {
-            return len;
-        }
-        else {
-            return size;
-        }
+        int n = set.size();
+        int len = nums.length;
+        
+        return n >= (len / 2) ? (len / 2) : n;
     }
 }
