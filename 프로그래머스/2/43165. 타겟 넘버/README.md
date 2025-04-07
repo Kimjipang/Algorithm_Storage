56aaa00892f7d11d9d545dc25cf506525769b1ff
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-04-07 13:16:46 +0900

    [level 2] Title: 타겟 넘버, Time: 94.81 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
 
 ### 성능 요약
 
-메모리: 99.6 MB, 시간: 797.08 ms
+메모리: 106 MB, 시간: 94.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 03일 23:28:23
+2025년 04월 07일 13:16:43
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,29 +1,34 @@
 import java.util.*;
 
 class Solution {
+    static ArrayDeque<Integer> queue;
     
     public int solution(int[] numbers, int target) {
         int answer = 0;
-        int numbers_len = numbers.length;
+        answer = bfs(numbers, target);
         
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(0);
+        return answer;
+    }
+    private int bfs(int[] numbers, int target) {
+        int len = numbers.length;
+        int result = 0;
         
-        for (int i = 0; i < numbers_len; i++) {
+        queue = new ArrayDeque<>();
+        queue.addLast(0);
+        
+        for (int i = 0; i < len; i++) {
             int size = queue.size();
             for (int j = 0; j < size; j++) {
-                int num = queue.poll();
-                queue.add(num + numbers[i]);
-                queue.add(num - numbers[i]);
+                int num = queue.pollFirst();
+                queue.addLast(num + numbers[i]);
+                queue.addLast(num - numbers[i]);
             }
         }
         
         while (!queue.isEmpty()) {
-            int num = queue.poll();
-            if (num == target) {
-                answer++;
-            }
+            if (queue.pollFirst() == target) result++;
         }
-        return answer;
+        
+        return result;
     }
 }
