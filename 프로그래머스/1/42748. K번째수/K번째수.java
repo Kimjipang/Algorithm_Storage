df493c84014ea05bf5fa33167d5d6b1af18f81c1
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-04-12 00:16:33 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,14 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
+        /*
+        array의 길이는 최대 100
+        commands의 길이는 50이하
+        */
+        int n = commands.length;
+        int[] answer = new int[n];
         
-        int len = commands.length;
-        int[] answer = new int[len];
-        
-        for(int i = 0; i < len; i++) {
-            int[] copy = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
-            Arrays.sort(copy);
-            answer[i] = copy[commands[i][2] - 1];
+        for (int i = 0; i < n; i++) {
+            int[] command = commands[i];
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int j = command[0] - 1; j < command[1]; j++) {
+                list.add(array[j]);
+            }
+            Collections.sort(list);
+            answer[i] = list.get(command[2] - 1);
         }
         
         return answer;
