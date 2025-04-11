378696c82750819929ca1531157a4f22bfab4af6
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-04-11 12:25:57 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.41 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,50 +1,49 @@
 import java.util.*;
 
 class Solution {
-    private static ArrayDeque<int[]> queue;
-    private static boolean[][] visited;
+    static ArrayDeque<int[]> queue;
+    static boolean[][] visited;
     
     public int solution(int[][] maps) {
         int answer = 0;
-        answer = bfs(maps);
         
+        answer = bfs(maps);
         return answer;
     }
     
-    private int bfs(int[][] maps) {
+    private static int bfs(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        List<Integer> goals = new ArrayList<>();
-        
         queue = new ArrayDeque<>();
         visited = new boolean[n][m];
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
         
         queue.addLast(new int[] {0, 0, 1});
         visited[0][0] = true;
         
         while (!queue.isEmpty()) {
-            int[] info = queue.pollFirst();
-            int x = info[0];
-            int y = info[1];
-            int count = info[2];
+            int[] arr = queue.pollFirst();
+            int curX = arr[0];
+            int curY = arr[1];
+            int count = arr[2];
             
-            if (x == n - 1 && y == m - 1) return count;
+            if (curX == n - 1 && curY == m - 1) {
+                return count;
+            }
             
             for (int i = 0; i < 4; i++) {
-                int next_x = x + dx[i];
-                int next_y = y + dy[i];
+                int nextX = curX + dx[i];
+                int nextY = curY + dy[i];
                 
-                if (next_x >= 0 && next_x < n && next_y >= 0 && next_y < m) {
-                    if (!visited[next_x][next_y] && maps[next_x][next_y] == 1) {
-                        visited[next_x][next_y] = true;
-                        queue.addLast(new int[] {next_x, next_y, count + 1});
+                if (nextX >= 0 && nextY >= 0 && nextX <= n - 1 && nextY <= m - 1) {
+                    if (!visited[nextX][nextY] && maps[nextX][nextY] == 1) {
+                        visited[nextX][nextY] = true;
+                        queue.addLast(new int[] {nextX, nextY, count + 1});
                     }
                 }
             }
         }
-        
         return -1;
     }
 }
