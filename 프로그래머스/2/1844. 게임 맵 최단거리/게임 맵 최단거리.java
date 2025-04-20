9cc1c1ebea26940dd1401a463da7b220fe13a708
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-04-21 00:23:08 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.88 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,42 +1,37 @@
 import java.util.*;
 
 class Solution {
-    static ArrayDeque<int[]> queue;
-    static boolean[][] visited;
-    
-    public int solution(int[][] maps) {
-        int answer = 0;
-        
-        answer = bfs(maps);
-        return answer;
-    }
+    private static ArrayDeque<int[]> queue;
+    private static boolean[][] visited;
     
     private static int bfs(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
-        queue = new ArrayDeque<>();
-        visited = new boolean[n][m];
+        
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
+        
+        queue = new ArrayDeque<>();
+        visited = new boolean[n][m];
         
         queue.addLast(new int[] {0, 0, 1});
         visited[0][0] = true;
         
         while (!queue.isEmpty()) {
             int[] arr = queue.pollFirst();
+            
             int curX = arr[0];
             int curY = arr[1];
             int count = arr[2];
             
-            if (curX == n - 1 && curY == m - 1) {
+            if (curX == n - 1 && curY == m - 1)
                 return count;
-            }
             
             for (int i = 0; i < 4; i++) {
                 int nextX = curX + dx[i];
                 int nextY = curY + dy[i];
                 
-                if (nextX >= 0 && nextY >= 0 && nextX <= n - 1 && nextY <= m - 1) {
+                if (nextX >= 0 && nextY >= 0 && nextX < n && nextY < m) {
                     if (!visited[nextX][nextY] && maps[nextX][nextY] == 1) {
                         visited[nextX][nextY] = true;
                         queue.addLast(new int[] {nextX, nextY, count + 1});
@@ -44,6 +39,13 @@ class Solution {
                 }
             }
         }
+        
         return -1;
+    }
+    public int solution(int[][] maps) {
+        int answer = 0;
+        
+        answer = bfs(maps);
+        return answer;
     }
 }
