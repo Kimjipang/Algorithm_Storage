7f19a0161c023f734a18f1bbd3acf9a99044e03f
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-04-28 23:41:21 +0900

    [level 3] Title: 섬 연결하기, Time: 0.66 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -12,37 +12,35 @@ class Solution {
     }
     
     private static void union(int x, int y) {
-        int root1 = find(x);
-        int root2 = find(y);
+        int num1 = find(x);
+        int num2 = find(y);
         
-        parent[root2] = root1;
+        parent[num2] = num1;
     }
-    
     public int solution(int n, int[][] costs) {
         Arrays.sort(costs, (o1, o2) -> Integer.compare(o1[2], o2[2]));
+        
+        int answer = 0;
+        int edge = 0;
         parent = new int[n];
         
         for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
         
-        int answer = 0;
-        int edge = 0;
-        
         for (int[] cost : costs) {
+            int node1 = cost[0];
+            int node2 = cost[1];
             
-            if (edge == n - 1) 
-                return answer;
+            if (edge == n - 1) break;
             
-            
-            if (find(cost[0]) != find(cost[1])) {
-                union(cost[0], cost[1]);
+            if (find(node1) != find(node2)) {
+                union(node1, node2);
                 
-                answer += cost[2];
                 edge++;
+                answer = answer + cost[2];
             }
         }
-        
         
         return answer;
     }
