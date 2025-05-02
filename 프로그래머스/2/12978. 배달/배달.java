147d318e2cccbe08ed5348132ef731c06a7907a3
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-02 13:30:33 +0900

    [level 2] Title: 배달, Time: 3.54 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,65 +1,59 @@
 import java.util.*;
 
 class Solution {
+    /*
+    양방향이라는 것 주의
+    */
     private static ArrayList<int[]>[] adjList;
+    private static PriorityQueue<int[]> pq;
     private static int[] dist;
     
-    private static void calculateDist() {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
-        
-        pq.add(new int[] {1, 0});
+    private static void checkAvailable() {
+        pq.add(new int[] {1 , 0});
         dist[1] = 0;
         
         while (!pq.isEmpty()) {
-            int[] arr = pq.poll();
-            int dest = arr[0];
-            int cost = arr[1];
+            int[] now = pq.poll();
+            int nowTown = now[0];
+            int nowDist = now[1];
+            if (nowDist > dist[nowTown]) continue;
             
-            if (dist[dest] < cost) {
-                continue;
-            }
-            
-            for (int[] next : adjList[dest]) {
-                if (dist[next[0]] > cost + next[1]) {
-                    dist[next[0]] = cost + next[1];
-                    pq.add(new int[] {next[0], dist[next[0]]});
+            for (int[] next : adjList[nowTown]) {
+                int nextTown = next[0];
+                int nextDist = next[1];
+                if (dist[nextTown] > nowDist + nextDist) {
+                    dist[nextTown] = nowDist + nextDist;
+                    pq.add(new int[] {nextTown, dist[nextTown]});
                 }
             }
         }
     }
+    
     public int solution(int N, int[][] road, int K) {
-        /*
-        N은 최대 50
-        road 최대 2,000
-        
-        1. 인접리스트 만들어서 각 마을까지 걸리는 시간을 Integer.MaxValue()로 잡는다
-        2. 다이렉트로 마을에 갈 수 있는 방법과 건너 갈 수 있는 방법 중 더 작은 것으로 값을 바꾼다.
-        3. K보다 작으면 count++ 하고 최종 반환
-        */
-        
         int answer = 0;
-        adjList = new ArrayList[N + 1];
-        dist = new int[N + 1];
         
-        int n = road.length;
+        adjList = new ArrayList[N + 1];
+        pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
+        dist = new int[N + 1];
         
         for (int i = 0; i <= N; i++) {
             adjList[i] = new ArrayList<>();
         }
         
-        for (int i = 0; i < n; i++) {
-            adjList[road[i][0]].add(new int[] {road[i][1], road[i][2]});
-            adjList[road[i][1]].add(new int[] {road[i][0], road[i][2]});
+        for (int[] arr : road) {
+            int from = arr[0];
+            int to = arr[1];
+            int cost = arr[2];
+            adjList[from].add(new int[] {to, cost});
+            adjList[to].add(new int[] {from, cost});
         }
         
         Arrays.fill(dist, Integer.MAX_VALUE);
         
-        calculateDist();
+        checkAvailable();
         
         for (int num : dist) {
-            if (num <= K) {
-                answer++;
-            }
+            if (num <= K) answer++;
         }
         
         return answer;
