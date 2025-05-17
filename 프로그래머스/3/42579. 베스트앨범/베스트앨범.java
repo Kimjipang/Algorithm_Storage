59b949e5d01d2d2958b67411a6957350f4b8bca4
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-17 23:30:43 +0900

    [level 3] Title: 베스트앨범, Time: 27.21 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,37 +2,53 @@ import java.util.*;
 import java.util.stream.*;
 
 class Solution {
-    public int[] solution(String[] genres, int[] plays) {
-
-        HashMap<String, ArrayList<int[]>> genreMap = new HashMap<>();
-        HashMap<String, Integer> playMap = new HashMap<>();
+    private static HashMap<String, Integer> genreMap;
+    private static HashMap<String, ArrayList<int[]>> playMap;
+    
+    private static int[] calculate(String[] genres, int[] plays) {
+        genreMap = new HashMap<>();
+        playMap = new HashMap<>();
+        
         int n = genres.length;
-
+        
         for (int i = 0; i < n; i++) {
             String genre = genres[i];
             int play = plays[i];
-
+            
             if (!genreMap.containsKey(genre)) {
-                genreMap.put(genre, new ArrayList<>());
-                playMap.put(genre, 0);
+                genreMap.put(genre, 0);
+                playMap.put(genre, new ArrayList<>());
             }
-            genreMap.get(genre).add(new int[] {i, play});
-            playMap.put(genre, playMap.get(genre) + play);
+            
+            genreMap.put(genre, genreMap.get(genre) + play);
+            playMap.get(genre).add(new int[] {i, play});
+            
         }
-
+        
         ArrayList<Integer> answer = new ArrayList<>();
-
-        Stream<Map.Entry<String, Integer>> sortedGenre = playMap.entrySet().stream()
-                                                                            .sorted((o1, o2) -> Integer.compare(o2.getValue(), o1.getValue()));
-
-        sortedGenre.forEach(entry -> {
-            Stream<int[]> sortedSongs = genreMap.get(entry.getKey()).stream()
-                                                                        .sorted((o1, o2) -> Integer.compare(o2[1], o1[1])).limit(2);
+        
+        // 장르별 재생수 내림차순 정렬
+        Stream<Map.Entry<String, Integer>> sortedGenres = genreMap.entrySet()
+                                    .stream()
+                                    .sorted((o1, o2)
+                                    -> Integer.compare(o2.getValue(), o1.getValue()));
+        
+        sortedGenres.forEach(entry -> {
+            Stream<int[]> sortedSongs = playMap.get(entry.getKey())
+                            .stream()
+                            .sorted((o1, o2) -> Integer.compare(o2[1], o1[1]))
+                            .limit(2);
+            
             sortedSongs.forEach(song -> answer.add(song[0]));
         });
-
+        
         return answer.stream()
-                        .mapToInt(Integer::intValue)
-                        .toArray();
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+        
+    }
+    public int[] solution(String[] genres, int[] plays) {
+        int[] answer = calculate(genres, plays);
+        return answer;
     }
 }
