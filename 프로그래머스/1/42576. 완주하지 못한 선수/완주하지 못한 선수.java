0b9c2bc05e68ad37df374f0e65f041b97054029e
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-17 12:00:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 339.28 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,25 +3,24 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         /*
-        participant의 길이는 최대 100,000이기에 O(N^2) 안됨.
+        participant 최대 길이는 100,000명이기 때문에 O(N^2) 불가
         
-        1. participant를 HashSet에 저장
-        2. completion 순회하면 HashSet에 있는지 확인
-        3. 동명이인 처리
         */
         
-        Map<String, Integer> map = new HashMap<>();
-        for (String str : completion) {
-            map.put(str, map.getOrDefault(str, 0) + 1);
-        }
+        Arrays.sort(participant);
+        Arrays.sort(completion);
         
-        for (String str : participant) {
-            if (map.getOrDefault(str, 0) == 0) {
-                return str;
+        int n = completion.length;
+        
+        for (int i = 0; i < n; i++) {
+            if (!completion[i].equals(participant[i])) {
+                return participant[i];
             }
-            map.put(str, map.get(str) - 1);
         }
         
-        return null;
+        String answer = "";
+        answer = participant[n];
+        
+        return answer;
     }
 }
