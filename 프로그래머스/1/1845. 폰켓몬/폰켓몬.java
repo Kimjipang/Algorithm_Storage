0210e5bdd80ac7cf7d3a8a4e0bae5208973e425e
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-17 12:39:46 +0900

    [level 1] Title: 폰켓몬, Time: 4.49 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,25 +1,20 @@
+
 import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        /*
-        nums의 길이는 최대 10,000이기에 O(N^2) ㄴㄴ
+        int answer = 0;
+        int n = nums.length;
         
-        [풀이]
-        1. 집합에 nums를 저장
-        2. 길이가 N/2이상이면 N/2 return
-        2-1. N/2 미만이면 집합의 길이 return
-        */
+        HashSet<Integer> pocketmon = new HashSet<>();
         
-        Set<Integer> set = new HashSet<>();
+        for (int num : nums) pocketmon.add(num);
         
-        for (int num : nums) {
-            set.add(num);
-        }
+        int size =  pocketmon.size();
+        if (size >= n / 2) return n / 2;
         
-        int n = set.size();
-        int len = nums.length;
+        answer = size;
         
-        return n >= (len / 2) ? (len / 2) : n;
+        return answer;
     }
 }
