f208c8d41df7f52174758b7480e2bda5805028de
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-20 16:27:50 +0900

    [level 2] Title: 행렬의 곱셈, Time: 21.41 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,18 +1,33 @@
 class Solution {
-    public int[][] solution(int[][] arr1, int[][] arr2) {
-        int row = arr1.length; // 3
-        int num = arr1[0].length;
-        int col = arr2[0].length; // 2
-        
-        int[][] answer = new int[row][col];
+    private static int row;
+    private static int row2;
+    private static int col;
+    private static int[][] answer;
+    
+    private static void calculate(int[][] arr1, int[][] arr2) {
         
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                for (int k = 0; k < num; k++) {
+                for (int k = 0; k < row2; k++) {
                     answer[i][j] += arr1[i][k] * arr2[k][j];
                 }
             }
         }
+    }
+    
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        /*
+        두 배열 모두 최대 100 X 100 길이의 배열 O(N^3)까지도 가능
+        [풀이]
+        행렬 곱셈은 행이랑 열을 곱함
+        */
+        row = arr1.length;
+        row2 = arr2.length;
+        col = arr2[0].length;
+                
+        answer = new int[row][col];
+        
+        calculate(arr1, arr2);
         return answer;
     }
 }
