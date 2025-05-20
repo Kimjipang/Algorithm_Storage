37d492455b5165047305cff32f99b237177bc6fa
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-20 15:59:04 +0900

    [level 1] Title: 모의고사, Time: 5.92 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,51 +1,50 @@
 import java.util.*;
 
 class Solution {
+    private static int[] personA = {1, 2, 3, 4, 5};
+    private static int[] personB = {2, 1, 2, 3, 2, 4, 2, 5};
+    private static int[] personC = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    
+    private static int calculateScore(int[] who, int[] answers) {
+        int n = answers.length;
+        int len = who.length;
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) { 
+            int answer = answers[i];
+            
+            if (who[i % len] == answer) count++;
+        }
+        return count;
+    }
     public int[] solution(int[] answers) {
         /*
-        answers가 주어지고 1,2,3번 수포자 중에서 누가 제일 많이 답을 맞췄는지 계산하는 문제
-        1번 수포자 -> 1, 2, 3, 4, 5 반복
-        2번 수포자 -> 2, 1, 2, 3, 2, 4, 2, 5 반복
-        3번 수포자 -> 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 반복
+        [풀이]
+        각 수포자가 찍는 방식을 담은 배열 초기화
+        answers 순회하면서 몇 개 맞았는지 체크
+        점수 같으면 오름차순 정렬
         
-        총 3번 순회를 해야 하는데, 3명의 수포자를 순회하면서 정답이랑 비교하고 정답 개수 반환
+        배열의 길이는 최대 10,000 O(N^2)으로 해결하면 안됨.
         */
-        int[] result = new int[3];
-        int len = answers.length;
+        ArrayList<Integer> result = new ArrayList<>();
         
-        int[][] people = {{1, 2, 3, 4, 5},
-                          {2, 1, 2, 3, 2, 4, 2, 5},
-                         {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+        int a = calculateScore(personA, answers); // n
+        int b = calculateScore(personB, answers); // n
+        int c = calculateScore(personC, answers); // n
         
-        for (int i = 0; i < 3; i++) {
-            int num = people[i].length;
-            int count = 0;
-            
-            for (int j = 0; j < len; j++) {
-                if (answers[j] == people[i][j % num]) {
-                    count++;
-                }
-            }
-            result[i] = count;
+        int[] scores = {a, b, c};
+        
+        int max = Arrays.stream(scores) // n
+                        .max()
+                        .getAsInt();
+        
+        for (int i = 0; i < 3; i++) { // 1
+            if (max == scores[i]) result.add(i + 1);
         }
+                
+        return result.stream() // n
+                    .mapToInt(Integer::intValue)
+                    .toArray();
         
-        return countBestScorer(result).stream()
-                                    .mapToInt(Integer::intValue)
-                                    .toArray();
-    }
-    
-    private static List<Integer> countBestScorer(int[] result) {
-        // 3개 중에서 제일 높은 숫자를 찾은 후에 다시 처음부터 비교해서 카운트를 세
-        List<Integer> list = new ArrayList<>();
-        
-        int num = Arrays.stream(result).max().orElse(-1);
-        
-        for (int i = 0; i < 3; i++) {
-            if (num == result[i]) {
-                list.add(i + 1);
-            }
-        }
-        
-        return list;
     }
 }
