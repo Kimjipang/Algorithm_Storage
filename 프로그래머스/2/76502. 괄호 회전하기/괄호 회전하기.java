c5c473584e07fac23574027eb3bdef66fe52d6d5
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-22 18:02:16 +0900

    [level 2] Title: 괄호 회전하기, Time: 25.28 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,40 +1,57 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String s) {
-        int answer = 0;
+    private static ArrayList<Character> list;
+    private static ArrayDeque<Character> stack;
+    private static int n;
+    
+    private static boolean isPossible() {
+        stack = new ArrayDeque<>();
         
-        ArrayDeque<Character> stack = new ArrayDeque<>();
-        int len = s.length();
-        
-        for (int i = 0; i < len; i++) {
-            if (isPossible(s, len)) {
-                answer++;
-            }
+        for (int i = 0; i < n; i++) {
+            char ch = list.get(i);
             
-            s = shift(s);
-        }
-        return answer;
-    }
-    private static boolean isPossible(String s, int len) {
-        ArrayDeque<Character> stack = new ArrayDeque<>();
-        
-        for (int i = 0; i < len; i++) {
-            char ch = s.charAt(i);
-            if (ch == '(' || ch == '{' || ch == '[') { // 열린 괄호면 스택에 push
-                stack.push(ch);
-            }
-            else { // 닫힌 괄호의 경우
+            if (ch == ']' || ch == ')' || ch == '}') {
                 if (stack.isEmpty()) return false;
-                else if (stack.peek() == '(' && ch == ')') stack.pop();
-                else if (stack.peek() == '[' && ch == ']') stack.pop();
-                else if (stack.peek() == '{' && ch == '}') stack.pop();
-                else return false;
+                else {
+                    char c = stack.peek();
+                    if (ch == ']' && c == '[') stack.pop();
+                    else if (ch == ')' && c == '(') stack.pop();
+                    else if (ch == '}' && c == '{') stack.pop();
+                }
             }
+            else stack.push(ch);
         }
-        return stack.isEmpty();
+        
+        return stack.size() == 0 ? true : false;
     }
-    private static String shift(String s) {
-        return s.substring(1) + s.charAt(0);
+    public int solution(String s) {
+        /*
+        s 최대 길이는 1,000이기에 O(N^2) 초과하는 시간복잡도는 불가
+        
+        [풀이]
+        stack 선언해서 s를 전부 push
+        pop하면서 올바른 괄호 여부 확인
+        
+        한 번 확인 끝났으면 앞에 인덱스 요소 제거해서 뒤로 add
+        */
+        
+        int answer = 0;
+        n = s.length();
+        list = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) { // n
+            list.add(s.charAt(i));
+        }
+        
+        for (int i = 0 ; i < n; i++) { // n^2
+            char ch = list.get(0);
+            
+            if (isPossible()) answer++;
+            list.remove(0);
+            list.add(ch);
+        }
+        
+        return answer;
     }
 }
