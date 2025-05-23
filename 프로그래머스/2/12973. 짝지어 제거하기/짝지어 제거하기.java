c31349030c6edb0e81efe8671e16fa4c51a398f2
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-23 21:45:45 +0900

    [level 2] Title: 짝지어 제거하기, Time: 58.87 ms, Memory: 64.7 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,23 +1,34 @@
 import java.util.*;
 
 class Solution {
+    private static ArrayDeque<Character> stack;
+    
     public int solution(String s) {
         /*
-        s가 최대 1,000,000이기에 O(N^2)는 안됨.
-        */
-        int answer = 0;
-        int len = s.length();
-        ArrayDeque<Character> stack = new ArrayDeque<>();
+        문자열 길이 최대 100만이기에 O(N^2) 미만의 시간 복잡도로 해결해야 함.
         
-        for (int i = 0; i < len; i++) {
-            char ch = s.charAt(i);
+        [풀이]
+        • 문자열 순회하면서 비어있으면 비어있으면 push()
+        • 비어있지 않으면 peek()해서 push()할 문자와 비교해서 같으면 pop() 다르면 push()
+        • 순회 끝났을 때 return isEmpty() ? 1 : 0;
+        
+        시간 복잡도 » O(N)
+        */
+        stack = new ArrayDeque<>();
+        
+        int answer = 0;
+        int n = s.length();
+        
+        for (int i = 0; i < n; i++) {
+            if (stack.isEmpty()) stack.push(s.charAt(i));
             
-            if (!stack.isEmpty() && stack.peekLast() == ch) stack.pollLast();
-            else stack.addLast(ch);
+            else {
+                if (stack.peek() == s.charAt(i)) stack.pop();
+                else stack.push(s.charAt(i));
+            }
         }
         
-        if (stack.isEmpty()) return 1;
-        
-        return answer;
+
+        return stack.isEmpty() ? 1 : 0;
     }
 }
