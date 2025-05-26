e42fbd980220291f6fe089e43ff2c99b6af2e14a
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-26 19:08:38 +0900

    [level 2] Title: 기능개발, Time: 3.73 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 75.5 MB, 시간: 4.14 ms
+메모리: 71.9 MB, 시간: 3.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 27일 10:53:13
+2025년 05월 26일 19:08:34
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,46 +1,57 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        /*
-        두 배열의 길이가 모두 최대 100이라 O(N^3)까지도 가능
-        
-        1. 각 기능 별로 얼마나 걸리는지를 계산해서 큐에 저장
-        2. 맨 앞 요소부터 꺼내면서 뒤에 요소보다 크면 모두 한 번에 반환
-        2-1. 뒤에 요소보다 작으면 뒤에 요소 전까지만 반환
-        */
+    private static ArrayDeque<Integer> deque;
+    
+    private static void calculate(int[] progresses, int[] speeds) {
         int n = progresses.length;
-        int[] answer = new int[n];
-        List<Integer> deploy = new ArrayList<>();
         
-        // 기능 구현 소요 시간 계산
         for (int i = 0; i < n; i++) {
-            int percent = progresses[i];
+            int progress = progresses[i];
             int speed = speeds[i];
             int count = 0;
-            while (percent < 100) {
-                percent += speed;
+            
+            while (progress < 100) {
+                progress += speed;
+
                 count++;
             }
-            answer[i] = count;
-        }
-        
-        // 배포 개수 계산
-        int num = answer[0];
-        int count = 1;
-        for (int i = 1; i < n; i++) {
-            if (num < answer[i]) {
-                deploy.add(count);
-                num = answer[i];
-                count = 1;
-            }
-            else count++;
+            deque.addLast(count);
             
         }
-        deploy.add(count);
         
-        return deploy.stream()
-                        .mapToInt(Integer::intValue)
-                        .toArray();
+    }
+    
+    public int[] solution(int[] progresses, int[] speeds) {
+        /*
+        두 배열 모두 최대 길이 100이기에 O(N^3) 이하의 시간 복잡도로 해결하면 됨.
+        
+        [풀이]
+        각 얼마나 걸리는지를 담는 배열을 초기화
+        */
+        deque = new ArrayDeque<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        calculate(progresses, speeds);
+        
+        int num = deque.pollFirst();
+        int count = 1;
+        
+        while (!deque.isEmpty()) {
+            int num2 = deque.pollFirst();
+            
+            if (num >= num2) count++;
+            else {
+                list.add(count);
+                num = num2;
+                count = 1;
+            }
+        }
+        
+        list.add(count);
+                
+        return list.stream()
+                    .mapToInt(Integer::intValue)
+                    .toArray();
     }
 }
