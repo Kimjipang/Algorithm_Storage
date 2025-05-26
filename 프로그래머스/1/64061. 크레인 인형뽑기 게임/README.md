1cc68d929ddd28008d5f97a1646fee9b34628738
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-26 17:21:52 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.27 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크레인 인형뽑기 게임 - 64061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061) 
 
 ### 성능 요약
 
-메모리: 82.7 MB, 시간: 0.82 ms
+메모리: 90.1 MB, 시간: 1.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 26일 11:38:00
+2025년 05월 26일 17:21:31
 
 ### 문제 설명
 
--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,42 +1,48 @@
 import java.util.*;
 
 class Solution {
+    private static ArrayDeque<Integer> stack;
+    private static int N;
+    
+    private static int play(int[][] board, int position) {
+        int peeked = 0;
+        int count = 0;
+        
+        for (int i = 0; i < N; i++) { // N 
+            if (board[i][position - 1] != 0) {
+                peeked = board[i][position - 1];
+                board[i][position - 1] = 0;
+                break;
+            }
+        }
+        
+        if (peeked != 0) {
+            if (!stack.isEmpty()) {
+                if (stack.peek() == peeked) {
+                    stack.pop();
+                    count += 2;
+                }
+                else stack.push(peeked);
+            }
+            else stack.push(peeked);
+        }
+        
+        return count;
+    }
+    
     public int solution(int[][] board, int[] moves) {
         /*
-        board의 길이는 최대 30, O(N^2) 가능
-        board의 요소가 0이면 인형 없는 것이고 숫자 같으면 같은 인형으로 간주
+        board 최대 길이 30 X 30
+        moves 최대 길이 1,000
         
-        1. moves를 순회하면서 board에서 값을 뺌.
-        2. 스택에 값이 없으면 그냥 넣고, 있으면 peek()으로 stack의 top 요소랑 비교
-        2-1). 비교해서 같으면 pop(), 다르면 add()
-        
+        최대 900,000 연산 횟수
         */
-        
         int answer = 0;
-        int len = moves.length;
-        int N = board.length;
-    
-        ArrayDeque<Integer> stack = new ArrayDeque<>(); // 바구니
+        stack = new ArrayDeque<>();
+        N = board.length;
         
-        for (int i = 0; i < len; i++) {
-            int col = moves[i] - 1;
-            for (int row = 0; row < N; row++) {
-                int doll = board[row][col];
-
-                if (doll != 0) {
-                    if (!stack.isEmpty() && stack.peek() == doll) {
-                        stack.pop();
-                        board[row][col] = 0;
-                        answer += 2;
-                        break;
-                    }
-                    else {
-                        stack.push(doll);
-                        board[row][col] = 0;
-                        break;
-                    }
-                } 
-            }
+        for (int move : moves) { // 최대 1,000
+            answer += play(board, move);
         }
         
         return answer;
