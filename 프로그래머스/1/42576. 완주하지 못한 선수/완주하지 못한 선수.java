e870086364cc47cd800cd7cf2eeb624574e7f3f4
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-28 00:29:01 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 103.03 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -6,25 +6,20 @@ class Solution {
         participant의 최대 길이는 100,000임. 시간 복잡도 O(N^2) 미만으로 해결 필수
         
         [풀이]
-        1. participant와 completion을 정렬한다.
-        2. completion 길이만큼 순회하면서 같은 인덱스 요소에 값이 다르면 완주하지 못한 선수임.
+        1. participant의 요소를 key로, 요소의 개수를 value로 하는 해시맵 초기화
+        2. completion를 순회하며 key가 존재하면 value를 -1
+        3. completion을 순회하며 value가 1이 아닌 것을 반환
         */
-        Arrays.sort(participant);
-        Arrays.sort(completion);
         
-        int n = completion.length;
+        HashMap<String, Integer> map = new HashMap<>();
         
-        for (int i = 0; i < n; i++) { // N 최대 100,000
-            String participantPerson = participant[i];
-            String completionPerson = completion[i];
-            
-            if (!participantPerson.equals(completionPerson)) {
-                return participantPerson;
-            }
+        for (String str : participant) map.put(str, map.getOrDefault(str, 0) + 1);
+        for (String str : completion) map.put(str, map.get(str) - 1);
+        
+        for (String str : participant) {
+            if (map.get(str) != 0) return str;
         }
-            
-        String answer = participant[n];
         
-        return answer;
+        return "";
     }
 }
