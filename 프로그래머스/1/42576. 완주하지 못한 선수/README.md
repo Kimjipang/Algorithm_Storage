ce6ff1a3b9269582d8e0cb481d0d05f6dcc975a1
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-28 00:22:13 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 330.43 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 96.6 MB, 시간: 339.28 ms
+메모리: 96.9 MB, 시간: 330.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 17일 12:00:18
+2025년 05월 28일 00:22:08
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,23 +3,27 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         /*
-        participant 최대 길이는 100,000명이기 때문에 O(N^2) 불가
+        participant의 최대 길이는 100,000임. 시간 복잡도 O(N^2) 미만으로 해결 필수
         
+        [풀이]
+        1. participant와 completion을 정렬한다.
+        2. completion 길이만큼 순회하면서 같은 인덱스 요소에 값이 다르면 완주하지 못한 선수임.
         */
-        
         Arrays.sort(participant);
         Arrays.sort(completion);
         
         int n = completion.length;
         
-        for (int i = 0; i < n; i++) {
-            if (!completion[i].equals(participant[i])) {
-                return participant[i];
+        for (int i = 0; i < n; i++) { // N 최대 100,000
+            String participantPerson = participant[i];
+            String completionPerson = completion[i];
+            
+            if (!participantPerson.equals(completionPerson)) {
+                return participantPerson;
             }
         }
-        
-        String answer = "";
-        answer = participant[n];
+            
+        String answer = participant[n];
         
         return answer;
     }
