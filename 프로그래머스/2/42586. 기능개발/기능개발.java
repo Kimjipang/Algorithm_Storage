200d11a6aca3663fc5eb50b221e63ddbf4525d9e
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-27 16:05:28 +0900

    [level 2] Title: 기능개발, Time: 5.05 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -4,6 +4,7 @@ class Solution {
     private static ArrayDeque<Integer> deque;
     
     private static void calculate(int[] progresses, int[] speeds) {
+        deque = new ArrayDeque<>();
         int n = progresses.length;
         
         for (int i = 0; i < n; i++) {
@@ -13,44 +14,38 @@ class Solution {
             
             while (progress < 100) {
                 progress += speed;
-
                 count++;
             }
-            deque.addLast(count);
             
+            deque.addLast(count);
         }
+        
         
     }
     
     public int[] solution(int[] progresses, int[] speeds) {
-        /*
-        두 배열 모두 최대 길이 100이기에 O(N^3) 이하의 시간 복잡도로 해결하면 됨.
         
-        [풀이]
-        각 얼마나 걸리는지를 담는 배열을 초기화
-        */
-        deque = new ArrayDeque<>();
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> answer = new ArrayList<>();
         
         calculate(progresses, speeds);
         
-        int num = deque.pollFirst();
+        int num1 = deque.pollFirst();
         int count = 1;
         
         while (!deque.isEmpty()) {
             int num2 = deque.pollFirst();
             
-            if (num >= num2) count++;
-            else {
-                list.add(count);
-                num = num2;
+            if (num1 < num2) {
+                num1 = num2;
+                answer.add(count);
                 count = 1;
             }
+            else count++;
         }
         
-        list.add(count);
-                
-        return list.stream()
+        answer.add(count);
+        
+        return answer.stream()
                     .mapToInt(Integer::intValue)
                     .toArray();
     }
