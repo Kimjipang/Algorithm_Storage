5d05a10e2128ba78d95aefe08cbd64aff543bbf6
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-28 15:33:15 +0900

    [level 2] Title: 할인 행사, Time: 136.64 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,38 +1,47 @@
 import java.util.*;
 
 class Solution {
+    private static HashMap<String, Integer> fruits;
+    private static int n;
+    private static int total;
+    
+    private static void initHashMap(String[] want, int[] number) {
+        fruits = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            fruits.put(want[i], number[i]);
+        }    
+    }
+    
     public int solution(String[] want, int[] number, String[] discount) {
         /*
-        want -> 원하는 제품
-        number -> 제품의 수량
+        want, number의 최대 길이 10
+        discount의 최대 길이 100,000
+        discount * number의 총합 최대치가 10,000,000 이기에 시간 복잡도면에서 가능함.
         
-        
-        1. want를 key, number를 value로 하는 map을 생성
-        2. discount를 0번째 요소부터 discount.length - number 총 수 + 1까지 순회
+        [풀이]
+        1. want를 key, number를 value로 하는 hashmap을 초기화
+        2. discount를 순회하면서 number의 총 수만큼 key에 해당하는 value를 -1한다.
+        3. 이후 모든 value가 0이면 result++;
         */
-        
         int answer = 0;
-        int len = want.length;
-        int n = discount.length;
         
-        for (int i = 0; i < n - 10 + 1; i++) {
-            Map<String, Integer> map = new HashMap<>();
-            int count = 0;
-            
-            for (int k = 0; k < len; k++) {
-            map.put(want[k], number[k]);
-            }
+        n = want.length;
+        initHashMap(want, number);
+        
+        int num = discount.length;
+        
+        for (int i = 0; i <= num - 10; i++) {
+            HashMap<String, Integer> purchased = new HashMap<>();
             
             for (int j = i; j < i + 10; j++) {
-                if (map.containsKey(discount[j]) && map.get(discount[j]) != 0){
-                    map.put(discount[j], map.get(discount[j]) - 1);
+                String fruit = discount[j];
+                if (fruits.containsKey(fruit)) {
+                    purchased.put(fruit, purchased.getOrDefault(fruit, 0) + 1);
                 }
-                else break;
-                if (map.get(discount[j]) == 0) count++;
-                
             }
             
-            if (count == len) answer++;
+            if (fruits.equals(purchased)) answer++;
         }
         
         return answer;
