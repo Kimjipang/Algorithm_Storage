6307e01754383d803596b2e520f4fb6c37ee18a9
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-28 16:24:46 +0900

    [level 2] Title: 오픈채팅방, Time: 173.14 ms, Memory: 167 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,37 +1,57 @@
 import java.util.*;
 
 class Solution {
+    private static HashMap<String, String> userInfo;
+    
+    private static void initHashMap(String[] record) {
+        userInfo = new HashMap<>();
+        
+        for (String log : record) {
+            String[] arr = log.split(" ");
+            String action = arr[0];
+            
+            if (action.equals("Enter") || action.equals("Change")) {
+                String id = arr[1];
+                String name = arr[2];
+                userInfo.put(id, name);
+            }
+        }
+    }
+    
+    private static String[] createLog(String[] record) {
+        ArrayList<String> result = new ArrayList<>();
+        
+        for (String log : record) {
+            String[] arr = log.split(" ");
+            String action = arr[0];
+            String id = arr[1];
+            
+            if (action.equals("Enter")) {
+                String name = userInfo.get(id);
+                result.add(name + "님이 들어왔습니다.");
+            }
+            else if (action.equals("Leave")) {
+                String name = userInfo.get(id);
+                result.add(name + "님이 나갔습니다.");
+            }
+        }
+        
+        return result.stream()
+                    .toArray(String[]::new);
+    }
+    
     public String[] solution(String[] record) {
         /*
-        record의 길이가 최대 100,000이기 때문에 O(N^2)는 안됨.
+        record의 최대 길이 100,000 O(N^2) 불가
         
         [풀이]
-        1. 유저 아이디가 key이고 닉네임이 value인 map을 생성
-        2. 
+        1. record를 순회하면서 uid를 key로, 이름을 value로 하는 HashMap을 초기화
+        2. 다시 순회를 하며 Enter면 ~님이 들어왔습니다. Leave면 나갔습니다 출력
         */
-        int n = record.length;
-        List<String> answer = new ArrayList<>();
+        initHashMap(record);
         
-        Map<String, String> member = new HashMap<>();
+        String[] answer = createLog(record);
         
-        // 멤버 정보 해시맵에 저장
-        for (String str : record) {
-            String[] log = str.split(" ");
-            if (!log[0].equals("Leave")) {
-                member.put(log[1], log[2]);
-            }
-        }
-        
-        for (int i = 0; i < n; i++) {
-            String[] log = record[i].split(" ");
-            if (log[0].equals("Enter")) {
-                answer.add(member.get(log[1]) + "님이 들어왔습니다.");
-            }
-            else if (log[0].equals("Leave")) {
-                answer.add(member.get(log[1]) + "님이 나갔습니다.");
-            }
-        }
-        
-        return answer.toArray(new String[0]);
+        return answer;
     }
 }
