93994cd7640b7e9b0e7a4d6b6fe64b49fe6158a2
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-05-29 18:22:29 +0900

    [level 3] Title: 베스트앨범, Time: 9.24 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 88.3 MB, 시간: 6.56 ms
+메모리: 73.6 MB, 시간: 9.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 17일 23:38:18
+2025년 05월 29일 18:22:24
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -5,50 +5,60 @@ class Solution {
     private static HashMap<String, Integer> genreMap;
     private static HashMap<String, ArrayList<int[]>> playMap;
     
-    private static int[] calculate(String[] genres, int[] plays) {
+    private static void initHashMap(String[] genres, int[] plays) {
+        int n = genres.length;
+        
         genreMap = new HashMap<>();
         playMap = new HashMap<>();
-        
-        int n = genres.length;
         
         for (int i = 0; i < n; i++) {
             String genre = genres[i];
             int play = plays[i];
             
-            if (!genreMap.containsKey(genre)) {
-                genreMap.put(genre, 0);
+            if (!playMap.containsKey(genre)) {
                 playMap.put(genre, new ArrayList<>());
             }
             
-            genreMap.put(genre, genreMap.get(genre) + play);
+            genreMap.put(genre, genreMap.getOrDefault(genre, 0) + play);
             playMap.get(genre).add(new int[] {i, play});
-            
         }
-        
+    }
+    
+    private static int[] generateBestAlbum() {
         ArrayList<Integer> answer = new ArrayList<>();
         
-        // 장르별 재생수 내림차순 정렬
-        Stream<Map.Entry<String, Integer>> sortedGenres = genreMap.entrySet()
-                                    .stream()
-                                    .sorted((o1, o2)
-                                    -> Integer.compare(o2.getValue(), o1.getValue()));
-        
-        sortedGenres.forEach(entry -> {
-            Stream<int[]> sortedSongs = playMap.get(entry.getKey())
+        Stream<Map.Entry<String, Integer>> sortedGenre = genreMap.entrySet()
                                 .stream()
-                                .sorted((o1, o2) -> Integer.compare(o2[1], o1[1]))
-                                .limit(2);
-            
+                                .sorted((o1, o2) ->
+                                    Integer.compare(o2.getValue(), o1.getValue()));
+        
+        sortedGenre.forEach(entry -> {
+            Stream<int[]> sortedSongs =playMap.get(entry.getKey())
+                                        .stream()
+                                        .sorted((o1, o2) -> 
+                                            Integer.compare(o2[1], o1[1]))
+                                        .limit(2);
             sortedSongs.forEach(song -> answer.add(song[0]));
         });
-        
+           
         return answer.stream()
                     .mapToInt(Integer::intValue)
                     .toArray();
-        
     }
+
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = calculate(genres, plays);
+        /*
+        genres와 plays의 최대 길이는 10,000이기에 O(N^2) 미만의 시간복잡도로 해결
+        
+        [풀이]
+        1. genres 별 총 재생 횟수를 저장하는 hashmap초기화 후 정렬
+        2. 곡별 재생 횟수를 저장하는 hashmap 초기화
+        3. 장르 별로 최대 2개씩 베스트 앨범에 추가
+        */
+        initHashMap(genres, plays);
+        
+        int[] answer = generateBestAlbum();
+        
         return answer;
     }
 }
