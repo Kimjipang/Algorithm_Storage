3b8a8c3e827b9ba9f9f2ca846b170b58e65dd1d5
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-06-03 21:11:11 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,10 +1,10 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985) 
 
 ### 성능 요약
 
-메모리: 90.6 MB, 시간: 0.04 ms
+메모리: 99.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 10일 12:33:23
+2025년 06월 03일 21:11:06
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,19 +1,20 @@
-
 class Solution {
     public int solution(int n, int a, int b) {
         /*
-        참가자 수가 최대 1024^2 정도
+        게임 참가자 수 N, 참가자 번호 A, 경쟁자 번호 B
+        짝수라서 부전승 X
         
+        1, 2 / 3, 4 / 5, 6
         [풀이]
-        1. 한 라운드 당 절반씩 탈락하기 때문에 A와 B의 다음 번호는 (기존 번호 + 1 ) / 2
-        2. (기존 번호 + 1) / 2가 같을 때까지 반복
+        
         */
         int answer = 1;
         
         while ((a + 1) / 2 != (b + 1) / 2) {
-            answer += 1;
             a = (a + 1) / 2;
             b = (b + 1) / 2;
+            
+            answer++;
         }
 
         return answer;
