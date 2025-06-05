4f73e12fce23f48df45e57c62c5689149624673f
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-06-05 18:46:26 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 72.90 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,45 +1,68 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        /*
-        판매원은 수익금을 90%만 먹고 10%는 추천인에게 줌.
-        때문에 모든 판매원은 본인의 수익금 + 자신이 추천해준 사람의 수익금 10%도 먹음.
-        하지만 1원 단위에서 절사
-        
-        [풀이]
-        1. 추천받은 사람과 추천인의 관계를 나타내는 HashMap 생성 <추천받은 사람, 추천인>
-        2. seller가 몇 개 팔았는지를 나타내는 HashMap 생성 <seller, amount>
-        3. 직원들이 총 얼마를 벌었는지 나타내는 HashMap 생성 <enroll, 수익금>
-        4. seller와 amount를 순회하며 추천인이 있는지를 확인
-        4-1. 추천인이 있으면 10% 떼고, 추천인의 추천인이 있는지 확인을 반복
-        
-        */
-        HashMap<String, String> recommendation = new HashMap<>();
-        HashMap<String, Integer> total = new HashMap<>();
+    private static HashMap<String, String> referralMap;
+    private static HashMap<String, Integer> profit;
+    
+    private static void initHashMap(String [] enroll, String [] referral) {
+        referralMap = new HashMap<>();
+        profit = new HashMap<>();
         
         int n = enroll.length;
-        int s = seller.length;
-        int[] answer = new int[n];
         
-        for (int i = 0; i < n; i++) { // <추천받은 사람, 추천인> HashMap 초기화
-            recommendation.put(enroll[i], referral[i]);
+        for (int i = 0; i < n; i++) {
+            String enroller = enroll[i];
+            String referraler = referral[i];
+            
+            if (!referralMap.containsKey(enroller)) {
+                referralMap.put(enroller, referraler);
+                profit.put(enroller, 0);
+            }
         }
+    }
+    
+    private static void calculateProfit(String[] seller, int[] amount) {
+        int n = seller.length;
         
-        for (int i = 0; i < s; i++) {
-            String curSeller = seller[i];
+        for (int i = 0; i < n; i++) {
+            String person = seller[i]; // seller가 young일 때 
             int money = amount[i] * 100;
             
-            while (money > 0 && !curSeller.equals("-")) {
-                total.put(curSeller, total.getOrDefault(curSeller, 0) + money - money / 10);
-                
-                curSeller = recommendation.get(curSeller);
-                money /= 10;
+            while (money > 0 && !person.equals("-")) {    
+                profit.put(person, profit.get(person) + money - money / 10);
+                person = referralMap.get(person);
+                money = money / 10;
             }
         }
         
+        
+        
+    }
+    
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        /*
+        referral, enroll 최대 길이 10,000이고 길이가 같음
+        seller의 최대 길이 100,000
+        amount의 최대 길이 100,000
+        
+        [풀이]
+        자신의 추천인이 누군지 hashmap으로 초기화 key -> enroll, value -> referral
+        각자 수익금이 얼마인지 hashmap으로 초기화 key -> enroll, value -> 수익금
+        
+        • 수익금 계산
+        - seller 순회하면서 value가 -가 아니면 value에게 10% 떼어주고, key값을 value로 하는 key의 value가
+          또 -인지 아닌지를 반복해서 확인 while문 필요
+        - "-"이면 10% 떼어주기만 하고 끝
+        
+        */
+        int n = enroll.length;
+        int[] answer = new int[n];
+        
+        initHashMap(enroll, referral);
+        calculateProfit(seller, amount);
+        
         for (int i = 0; i < n; i++) {
-            answer[i] = total.getOrDefault(enroll[i], 0);
+            answer[i] = profit.get(enroll[i]);
         }
         
         return answer;
