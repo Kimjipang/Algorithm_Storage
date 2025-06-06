e48c04bf89b19d3540d35b3637cac3fc61069d77
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-06-06 13:33:12 +0900

    [level 3] Title: 네트워크, Time: 0.19 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,55 +1,31 @@
-import java.util.*;
-
 class Solution {
-    private static ArrayList<Integer>[] adjList;
-    private static ArrayDeque<Integer> queue;
     private static boolean[] visited;
-    
-    private static void bfs(int num) {
-        queue = new ArrayDeque<>();
-        
-        queue.addLast(num);
-        visited[num] = true;
-        
-        while (!queue.isEmpty()) {
-            int idx = queue.pollFirst();
-            int size = adjList[idx].size();
-            
-            if (size == 0) continue;
-            
-            for (int i = 0; i < size; i++) {
-                int node = adjList[idx].get(i);
-                if (!visited[node]) {
-                    queue.addLast(node);
-                    visited[node] = true;
-                }
+    private static int[][] computer;
+
+    private static void dfs(int start) {
+        visited[start] = true;
+
+        int n = computer[start].length;
+
+        for (int i = 0; i < n; i++) {
+            if (computer[start][i] == 1 && !visited[i]) {
+                dfs(i);
             }
         }
     }
-    
+
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        adjList = new ArrayList[n];
         visited = new boolean[n];
-        
-        for (int i = 0; i < n; i++) {
-            adjList[i] = new ArrayList<>();
-        }
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j && computers[i][j] == 1) 
-                adjList[i].add(j);
-            }
-        }
-        
+        computer = computers;
+        int answer = 0;
+
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                bfs(i);
+                dfs(i);
                 answer++;
             }
         }
-        
+
         return answer;
     }
 }
