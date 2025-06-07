cf5c9b68eb1ca5c1a882eea8fe854a9bf70e261a
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-06-08 00:27:37 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.56 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,65 +1,47 @@
 import java.util.*;
 
 class Solution {
-    private static ArrayDeque<Node> queue;
-    private static boolean[][] visited;
-    
-    public static class Node {
-        int x;
-        int y;
-        int count;
-        
-        public Node(int x, int y, int count) {
-            this.x = x;
-            this.y = y;
-            this.count = count;
-        }
-    }
-    
-    private static int bfs(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-        
-        queue = new ArrayDeque<>();
+    static int n, m;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+    static boolean[][] visited;
+
+    public int solution(int[][] maps) {
+        n = maps.length;
+        m = maps[0].length;
         visited = new boolean[n][m];
-        
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        
-        Node node = new Node(0, 0, 1);
-        
-        queue.addLast(node);
-        
-        visited[0][0] = true;
-        
-        while (!queue.isEmpty()) {
-            Node cur = queue.pollFirst();
-            int curX = cur.x;
-            int curY = cur.y;
-            int count = cur.count;
-            
-            if (curX == n - 1 && curY == m - 1) return count;
-            
+
+        return bfs(0, 0, maps);
+
+    }
+    private static int bfs(int x, int y, int[][] maps) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {x, y, 1});
+        visited[x][y] = true;
+
+
+        while(!queue.isEmpty()) {
+            int[] arr = queue.poll();
+            int curX = arr[0];
+            int curY = arr[1];
+            int count = arr[2];
+
+            if (curX == n - 1 && curY == m - 1) {
+                return count;
+            }
+
             for (int i = 0; i < 4; i++) {
                 int nextX = curX + dx[i];
                 int nextY = curY + dy[i];
-                
+
                 if (nextX >= 0 && nextY >= 0 && nextX < n && nextY < m) {
-                    if (!visited[nextX][nextY] && maps[nextX][nextY] == 1) {
+                    if (maps[nextX][nextY] == 1 && !visited[nextX][nextY]) {
+                        queue.add(new int[] {nextX, nextY, count + 1});
                         visited[nextX][nextY] = true;
-                        queue.add(new Node(nextX, nextY, count + 1));
                     }
                 }
             }
         }
         return -1;
-    }
-        
-    
-    public int solution(int[][] maps) {
-        int answer = 0;
-        
-        answer = bfs(maps);
-        return answer;
     }
 }
