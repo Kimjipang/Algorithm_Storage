7947c5b43d3e4ed86d87e8de114e50afa4f39c22
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-06-14 19:54:07 +0900

    [level 3] Title: 네트워크, Time: 0.65 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 73.9 MB, 시간: 0.19 ms
+메모리: 82.8 MB, 시간: 0.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 06일 13:33:07
+2025년 06월 14일 19:54:00
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,31 +1,59 @@
+import java.util.*;
+
 class Solution {
+    private static ArrayList<Integer>[] adjList;
+    private static ArrayDeque<Integer> queue;
     private static boolean[] visited;
-    private static int[][] computer;
-
-    private static void dfs(int start) {
-        visited[start] = true;
-
-        int n = computer[start].length;
-
-        for (int i = 0; i < n; i++) {
-            if (computer[start][i] == 1 && !visited[i]) {
-                dfs(i);
+    
+    private static void bfs(int i) {
+        visited[i] = true;
+        queue.addLast(i);
+        
+        while (!queue.isEmpty()) {
+            int cur = queue.pollFirst();
+            
+            for (int next : adjList[cur]) {
+                if (!visited[next]) {
+                    queue.addLast(next);
+                    visited[next] = true;
+                }
             }
         }
+        
     }
-
+    
     public int solution(int n, int[][] computers) {
+        /*
+        n, computers 최대 200
+        
+        [풀이]
+        컴퓨터 간 연결 정보 저장할 리스트 초기화
+        방문한 곳은 방문 처리
+        1번(인덱스 0) 컴퓨터부터 n번 컴퓨터까지 
+        */
+        adjList = new ArrayList[n];
         visited = new boolean[n];
-        computer = computers;
+        queue = new ArrayDeque<>();
         int answer = 0;
-
+        
+        for (int i = 0; i < n; i++) {
+            adjList[i] = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if (i != j && computers[i][j] == 1) {
+                    adjList[i].add(j);
+                }
+            }
+        }
+        
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                dfs(i);
+                bfs(i);
                 answer++;
             }
+                
         }
-
+        
+        
         return answer;
     }
 }
