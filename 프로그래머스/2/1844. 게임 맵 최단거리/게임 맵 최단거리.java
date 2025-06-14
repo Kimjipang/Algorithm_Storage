abeab695d3baa975e03a9bcd13f93547044933b8
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-06-14 15:53:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.02 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,36 +1,46 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[][] maps) {
-        int rows = maps.length;
-        int cols = maps[0].length;
-
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; 
-
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0, 0, 1});
-
+    private static ArrayDeque<int[]> queue;
+    private static boolean[][] visited;
+    
+    private static int bfs(int[][] maps) {
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        int n = maps.length;
+        int m = maps[0].length;
+        
+        queue = new ArrayDeque<>();
+        visited = new boolean[n][m];
+        
+        queue.addLast(new int[] {0, 0, 1});
+        visited[0][0] = true;
+        
         while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int row = current[0];
-            int col = current[1];
-            int distance = current[2];
-
-            if (row == rows - 1 && col == cols - 1) {
-                return distance; // 목적지에 도달한 경우 최단거리 반환
-            }
-
-            for (int[] dir : directions) {
-                int newRow = row + dir[0];
-                int newCol = col + dir[1];
-
-                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && maps[newRow][newCol] == 1) {
-                    maps[newRow][newCol] = 0;
-                    queue.offer(new int[]{newRow, newCol, distance + 1});
+            int[] arr = queue.pollFirst();
+            int curX = arr[0];
+            int curY = arr[1];
+            int count = arr[2];
+            
+            if (curX == n - 1 && curY == m - 1) return count;
+            
+            for (int i = 0; i < 4; i++) {
+                int nextX = curX + dx[i];
+                int nextY = curY + dy[i];
+                
+                if (nextX >= 0 && nextY >= 0 && nextX < n && nextY < m) {
+                    if (!visited[nextX][nextY] && maps[nextX][nextY] == 1) {
+                        visited[nextX][nextY] = true;
+                        queue.addLast(new int[] {nextX, nextY, count + 1});
+                    }
                 }
             }
         }
-
         return -1;
+    }
+    
+    public int solution(int[][] maps) {
+        int answer = bfs(maps);
+        return answer;
     }
 }
