b70d74040ae547c8a4a6aaf3721db502ed3d6b46
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-06-16 12:27:04 +0900

    [level 3] Title: 네트워크, Time: 0.76 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,57 +2,59 @@ import java.util.*;
 
 class Solution {
     private static ArrayList<Integer>[] adjList;
-    private static ArrayDeque<Integer> queue;
     private static boolean[] visited;
+    private static ArrayDeque<Integer> queue;
+    private static int N;
     
-    private static void bfs(int i) {
-        visited[i] = true;
-        queue.addLast(i);
+    private static void init(int[][] computers) {
+        visited = new boolean[N];
+        queue = new ArrayDeque<>();
+        adjList = new ArrayList[N];
+        
+        for (int i = 0; i < N; i++) {
+            adjList[i] = new ArrayList<>();
+            for (int j = 0; j < N; j++) {
+                if (i != j && computers[i][j] == 1) {
+                    adjList[i].add(j);
+                }
+            }
+        }
+    }
+    
+    private static void bfs(int start) {
+        queue.addLast(start);
+        visited[start] = true;
         
         while (!queue.isEmpty()) {
             int cur = queue.pollFirst();
             
             for (int next : adjList[cur]) {
                 if (!visited[next]) {
-                    queue.addLast(next);
                     visited[next] = true;
+                    queue.addLast(next);
                 }
             }
         }
-        
     }
-    
     public int solution(int n, int[][] computers) {
         /*
-        n, computers 최대 200
+        n이 최대 200이기에 computers의 최대 길이 순회 시 200 X 200
         
         [풀이]
-        컴퓨터 간 연결 정보 저장할 리스트 초기화
-        방문한 곳은 방문 처리
-        1번(인덱스 0) 컴퓨터부터 n번 컴퓨터까지 
-        */
-        adjList = new ArrayList[n];
-        visited = new boolean[n];
-        queue = new ArrayDeque<>();
-        int answer = 0;
+        인접 리스트 초기화 하고 bfs 돌리기
         
-        for (int i = 0; i < n; i++) {
-            adjList[i] = new ArrayList<>();
-            for (int j = 0; j < n; j++) {
-                if (i != j && computers[i][j] == 1) {
-                    adjList[i].add(j);
-                }
-            }
-        }
+        */
+        N = n;
+        init(computers);
+        
+        int answer = 0;
         
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 bfs(i);
                 answer++;
             }
-                
         }
-        
         
         return answer;
     }
