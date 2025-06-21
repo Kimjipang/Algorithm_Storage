1e822e04c4469dc4c257a4bd1c674bfd41b6585e
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-06-22 01:21:04 +0900

    [level 2] Title: 가장 큰 수, Time: 205.19 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 410 MB, 시간: 2965.30 ms
+메모리: 144 MB, 시간: 205.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 17일 00:02:31
+2025년 06월 22일 01:20:59
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,22 +2,20 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
+        /*
+        
+        */
         int n = numbers.length;
         String[] arr = new String[n];
         
         for (int i = 0; i < n; i++) {
-            arr[i] = "" + numbers[i];
+            arr[i] = String.valueOf(numbers[i]);
         }
         
         Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
         
         if (arr[0].equals("0")) return "0";
         
-        for (String str : arr) {
-            answer += str;
-        }
-        
-        return answer;
+        return String.join("", arr);
     }
 }
