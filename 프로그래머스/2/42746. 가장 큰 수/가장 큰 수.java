cbd0177d1cb069c8193570fb027471c8fb1d8f50
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-06-23 22:11:56 +0900

    [level 2] Title: 가장 큰 수, Time: 220.63 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -12,7 +12,7 @@ class Solution {
             arr[i] = String.valueOf(numbers[i]);
         }
         
-        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        Arrays.sort(arr, (o1, o2) -> Integer.compare(Integer.parseInt(o2 + o1), Integer.parseInt(o1 + o2)));
         
         if (arr[0].equals("0")) return "0";
         
