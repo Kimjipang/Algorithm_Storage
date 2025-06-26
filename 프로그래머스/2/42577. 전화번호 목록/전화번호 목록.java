87348edb06a2da0f9bd61f90c34e82a014196dd6
김지환 <91196025+Kimjipang@users.noreply.github.com>
2025-06-26 19:09:39 +0900

    [level 2] Title: 전화번호 목록, Time: 280.38 ms, Memory: 248 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,23 +2,12 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        /*
-        phone_book 최대 길이 1,000,000이기에 O(N^2) 미만으로 해결
+        HashSet<String> set = new HashSet<>(Arrays.asList(phone_book));
         
-        [풀이]
-        - 문자열 오름차순 정렬
-        - 앞뒤로 접두사인지 확인
-        */      
-        Arrays.sort(phone_book); // O(NlogN)
-        int num = phone_book.length;
-        
-        // System.out.println(Arrays.toString(phone_book));
-        
-        for (int i = 0; i < num - 1; i++) { // O(N)
-            String cur = phone_book[i];
-            String next = phone_book[i + 1];
-            
-            if (next.startsWith(cur)) return false;
+        for (String s : phone_book) {
+            for (int i = 1; i < s.length(); i++) {
+                if (set.contains(s.substring(0, i))) return false;
+            }
         }
 
         return true;
