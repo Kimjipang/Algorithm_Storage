d8da131b927e534c82ca42a4fc3b577d636a0c46
김지환 <91196025+Kimjipang@users.noreply.github.com>
2026-03-11 19:54:14 +0900

    [level 2] Title: 숫자의 표현, Time: 0.14 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,22 +1,19 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n) {
-        int answer = 1;
-        
-        if (n > 2) {
-            for (int i = 2; i < n; i++) {
-                if (i == 2) {
-                    if (n % 2 != 0) {
-                        answer++;
-                    }
-                }
-                else {
-                    if (i % 2 != 0 && n % i == 0) {
-                        answer++;
-                    }
-                }
+        /*
+        연속하는 수이기 때문에 짝수는 표현할 수 없음.
+        ex). 4, 6, 8은 모두 최소 2로는 나누어 떨어지는데 이들을 연속하는 수의 합으로 표현할 수 없음.
+        n부터 2로 나누어 보며 
+        */
+        int answer = 0;
+    
+        for (int i = 1; i <= n; i += 2) {
+            if (n % i == 0) {
+                answer++;
+            }
         }
-        }
-        
         return answer;
     }
 }
